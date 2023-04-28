cd71b8a4931687e54453385bf554436493950c4e
Thomas Schank <DrTom@schank.ch>
2023-04-28 21:00:50 +0200

    ...

--- cljc-src/madek/auth/resources/sign_in/auth_systems/auth_system_request/main.clj
+++ cljc-src/madek/auth/resources/sign_in/auth_systems/auth_system_request/main.clj
@@ -3,6 +3,7 @@
     [buddy.core.keys :as keys]
     [buddy.sign.jwt :as jwt]
     [honey.sql :refer [format] :rename {format sql-format}]
+    [madek.auth.resources.sign-in.auth-systems.sql :refer [auth-systems-query]]
     [honey.sql.helpers :as sql]
     [madek.auth.db.core :refer [get-ds]]
     [next.jdbc :as jdbc]
@@ -12,4 +13,12 @@
 
 (defn handler [{{email :email
                  auth_system_id :auth_system_id} :params tx :tx :as request}]
-  {:status 402})
+  (info 'request request)
+
+  (if-let [user-auth-system (-> email auth-systems-query spy
+                                (sql/where [:= :auth_systems.id auth_system_id])
+                                (sql-format :inline true)
+                                spy
+                                (#(jdbc/execute-one! tx %)))]
+    user-auth-system ; TODO create JWT
+    {:status 402}))
